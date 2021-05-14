2b6e9363e5f33db63088053987c81c8202ac44a9
t-liorkesten <t-liorkesten@microsoft.com>
2021-05-14 13:19:45 +0300

    ARG-proxy added
    ARG proxy was added.
    Order folders.

---
+++ src_pocs/proxy-arg/go.mod
@@ -0,0 +1,24 @@
+module github.com/Azure/opa-asc-proxy
+
+go 1.12
+
+require (
+	github.com/Azure/azure-sdk-for-go v39.1.0+incompatible
+	github.com/Azure/go-autorest/autorest v0.9.1
+	github.com/Azure/go-autorest/autorest/adal v0.9.13
+	github.com/Azure/go-autorest/autorest/date v0.3.0
+	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
+	github.com/open-policy-agent/frameworks/constraint v0.0.0-20200201011441-b56df07ae2b8 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/pflag v1.0.5
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed
+	google.golang.org/appengine v1.6.5
+	google.golang.org/grpc v1.27.0 // indirect
+	gopkg.in/yaml.v2 v2.2.4
+	k8s.io/apiextensions-apiserver v0.17.2 // indirect
+	k8s.io/klog v1.0.0
+)
---
+++ src_pocs/proxy-arg/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+
+	//"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+)
+
+var (
+	debug  = pflag.Bool("debug", true, "sets log to debug level")
+	server *Server
+	ctx    context.Context
+)
+
+// LogHook is used to setup custom hooks
+type LogHook struct {
+	Writer    io.Writer
+	Loglevels []log.Level
+}
+
+func main() {
+	//pflag.Parse()
+
+	var err error
+
+	setupLogger()
+
+	ctx = context.Background()
+	server, err = NewServer()
+	if err != nil {
+		log.Fatalf("[error] : %v", err)
+	}
+	http.HandleFunc("/process", handle)
+	http.ListenAndServe(":8090", nil)
+
+	os.Exit(0)
+}
+
+func handle(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	image := req.URL.Query().Get("image") // e.g. : oss/kubernetes/aks/etcd-operator
+	if image == "" {
+		log.Info("Failed to provide image to query")
+		w.WriteHeader(http.StatusForbidden)
+		json.NewEncoder(w).Encode(nil)
+	}
+
+	// registry := "upstream.azurecr.io"
+	// repo := "oss/kubernetes/ingress/nginx-ingress-controller"
+	// tag := "0.16.2"
+	registry := strings.Split(image, "/")[0]
+	repo := strings.Replace(image, registry+"/", "", 1)
+	tag := "latest"
+	if strings.Contains(repo, ":") {
+		tag = strings.Split(repo, ":")[1]
+		repo = strings.Replace(repo, ":"+tag, "", 1)
+	}
+
+	getImageShaBinary := "getimagesha.sh"
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Registry:", registry)
+	log.Info("Repo:", repo)
+	log.Info("Tag:", tag)
+
+	cmd := exec.Command(
+		"sh",
+		getImageShaBinary,
+		registry,
+		repo,
+		tag,
+	)
+	log.Infof("cmd: %v", cmd)
+	cmd.Dir = dir
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stderr, cmd.Stdout = stderr, stdout
+
+	err = cmd.Run()
+	output := stdout.String()
+	log.Infof("output: %s", output)
+	if err != nil {
+		log.Errorf("error invoking cmd, err: %v, output: %v", err, stderr.String())
+	}
+	if output == "null\n" {
+		log.Infof("[error] : could not find valid digest %s", output)
+		w.WriteHeader(http.StatusForbidden)
+		json.NewEncoder(w).Encode(nil)
+	} else {
+		digest := strings.TrimSuffix(output, "\n")
+		log.Infof("digest: %s", digest)
+
+		data, err := server.Process(ctx, digest)
+		if err != nil {
+			log.Infof("[error] : %s", err)
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(data)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(data)
+		}
+	}
+}
+
+// setupLogger sets up hooks to redirect stdout and stderr
+func setupLogger() {
+	log.SetOutput(ioutil.Discard)
+
+	// set log level
+	log.SetLevel(log.InfoLevel)
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	// add hook to send info, debug, warn level logs to stdout
+	log.AddHook(&LogHook{
+		Writer: os.Stdout,
+		Loglevels: []log.Level{
+			log.InfoLevel,
+			log.DebugLevel,
+			log.WarnLevel,
+		},
+	})
+
+	// add hook to send panic, fatal, error logs to stderr
+	log.AddHook(&LogHook{
+		Writer: os.Stderr,
+		Loglevels: []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+			log.ErrorLevel,
+		},
+	})
+}
+
+// Fire is called when logging function with current hook is called
+// write to appropriate writer based on log level
+func (hook *LogHook) Fire(entry *log.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	_, err = hook.Writer.Write([]byte(line))
+	return err
+}
+
+// Levels defines log levels at which hook is triggered
+func (hook *LogHook) Levels() []log.Level {
+	return hook.Loglevels
+}
