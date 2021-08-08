d19ad08f9f68e4baebf5e0c8c371ae6f0b8b4713
t-liorkesten <t-liorkesten@microsoft.com>
2021-08-08 18:01:13 +0300

    added trace to msg

--- pkg/infra/instrumentation/tracer.go
+++ pkg/infra/instrumentation/tracer.go
@@ -1,8 +1,10 @@
 package instrumentation
 
 import (
+	"fmt"
 	"github.com/go-logr/logr"
 	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 // ITracer interface of tracer that extends the logr.Logger interface
@@ -13,6 +15,7 @@ type ITracer struct {
 // Tracer implementation of ITracer interface - holds a Entry object in order to delegate Tivan tracer methods.
 type Tracer struct {
 	Entry *logrus.Entry
+	trace string
 }
 
 // Enabled tests whether this Logger is enabled.
@@ -21,9 +24,15 @@ func (tracer Tracer) Enabled() bool {
 	return tracer.Entry != nil
 }
 
+type X struct {
+	trace string
+	msg   string
+}
+
 // Info writes msg. delegate this method using Tracer.Entry data member in order to implement logr.Logger interface
 func (tracer Tracer) Info(msg string, keysAndValues ...interface{}) {
-	tracer.Entry.Info(msg, keysAndValues)
+	newMsg := tracer.constructMsg(msg)
+	tracer.Entry.Info(newMsg)
 }
 
 // Error writes error. delegate this method using Tracer.Entry data member in order to implement logr.Logger interface
@@ -43,5 +52,30 @@ func (tracer Tracer) WithValues(keysAndValues ...interface{}) logr.Logger {
 
 // WithName implements this method only for logr.Logger interface. doesn't do anything - return the same tracer!
 func (tracer Tracer) WithName(name string) logr.Logger {
-	return tracer
+	newTracer := tracer.Named(name)
+	return newTracer
+}
+
+// Named adds a new path segment to the logger's name. Segments are joined by
+// periods. By default, Loggers are unnamed.
+func (tracer *Tracer) Named(s string) *Tracer {
+	if s == "" {
+		return tracer
+	}
+	t := tracer.clone()
+	if tracer.trace == "" {
+		t.trace = s
+	} else {
+		t.trace = strings.Join([]string{t.trace, s}, ".")
+	}
+	return t
+}
+
+func (tracer *Tracer) clone() *Tracer {
+	copiedTracer := *tracer
+	return &copiedTracer
+}
+
+func (tracer *Tracer) constructMsg(msg string) (newMsg string) {
+	return fmt.Sprintf(`{"trace":"%s","msg":"%s"}`, tracer.trace, msg)
 }
