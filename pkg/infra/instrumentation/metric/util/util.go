d08f414ba0c88319a5179fbf2d2b17482c72b674
t-liorkesten <t-liorkesten@microsoft.com>
2021-09-05 18:52:38 +0300

    Changed GetDurationMilliseconds to get only startTime

--- pkg/infra/instrumentation/metric/util/util.go
+++ pkg/infra/instrumentation/metric/util/util.go
@@ -3,7 +3,10 @@ package util
 import "time"
 
 // GetDurationMilliseconds returns the duration between startTime and currentTime in milliseconds as int.
+// The function convert startTime to UTC. example calling:
+// startTime := Time.Now()
+// util.GetDurationMilliseconds(startTime)
 func GetDurationMilliseconds(startTime time.Time) int {
 	endTime := time.Now().UTC()
-	return int(startTime.Sub(endTime).Milliseconds())
+	return int(startTime.UTC().Sub(endTime).Milliseconds())
 }
