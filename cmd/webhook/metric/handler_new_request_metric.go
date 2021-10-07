91b88c5efff585a090677e99f5584125d3888c59
Tomer Weinberger <41298684+wtomw@users.noreply.github.com>
2021-10-07 16:30:21 +0300

    Handler error capability (#38)
    * Handler error capability
    
    * Handler error capability
    
    * fix merge
    
    * Fixed handler filter logic
    
    * Query ARG in subscription scope.
    
    * PR
    
    * Added deployment utils
    
    * PR
    
    * Added tests for deployment utils.
    
    * Renamed pod request
    
    Co-authored-by: t-liorkesten <t-liorkesten@microsoft.com>

--- cmd/webhook/metric/handler_new_request_metric.go
+++ cmd/webhook/metric/handler_new_request_metric.go
@@ -2,18 +2,22 @@ package metric
 
 import (
 	"github.com/Azure/AzureDefender-K8S-InClusterDefense/pkg/infra/instrumentation/metric"
+	admissionv1 "k8s.io/api/admission/v1"
 )
 
 // HandlerNewRequestMetric is metric for the requests kinds of the handler
 type HandlerNewRequestMetric struct {
 	// requestKing is the kind that the handler was got in the request (e.g. Pod).
 	requestKind string
+	// requestOperation is the operation type of the request (create/update)
+	requestOperation string
 }
 
 // NewHandlerNewRequestMetric Ctor
-func NewHandlerNewRequestMetric(kind string) *HandlerNewRequestMetric {
+func NewHandlerNewRequestMetric(kind string, operation admissionv1.Operation) *HandlerNewRequestMetric {
 	return &HandlerNewRequestMetric{
-		requestKind: kind,
+		requestKind:      kind,
+		requestOperation: string(operation),
 	}
 }
 
@@ -24,5 +28,6 @@ func (m *HandlerNewRequestMetric) MetricName() string {
 func (m *HandlerNewRequestMetric) MetricDimension() []metric.Dimension {
 	return []metric.Dimension{
 		{Key: "RequestKind", Value: m.requestKind},
+		{Key: "RequestOperation", Value: m.requestOperation},
 	}
 }
