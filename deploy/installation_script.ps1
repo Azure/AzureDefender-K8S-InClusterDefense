b8529148f38bac4ccdc1e1220417bbb15ec80d23
t-liorkesten <t-liorkesten@microsoft.com>
2021-11-07 20:58:06 +0200

    Updated installation script
    Signed-off-by: t-liorkesten <t-liorkesten@microsoft.com>

--- deploy/installation_script.ps1
+++ deploy/installation_script.ps1
@@ -36,17 +36,39 @@ Function PrinitNewSection($stepTitle)
 #                                   Extract used variables
 # Extract the region of the cluster
 $region = az aks show --resource-group $resource_group --name $cluster_name --query location
+if ($LASTEXITCODE -eq 3)
+{
+    write-error "Failed to get the region of the cluster"
+    exit $LASTEXITCODE
+}
 # Create node resource group variable - MC_<RESOURCE_GROUP>_<CLUSTER_NAME>_<REGION>
 $node_resource_group = az aks show --resource-group $resource_group --name $cluster_name --query nodeResourceGroup
+if ($LASTEXITCODE -eq 3)
+{
+    write-error "Failed to get the node resource group of the cluster"
+    exit $LASTEXITCODE
+}
 # Create managed identity name
 $in_cluster_defense_identity_name = "AzureDefenderInClusterDefense-$cluster_name"
 # Extract kubelet identity
 $kubelet_client_id = az aks show --resource-group $resource_group --name $cluster_name --query identityProfile.kubeletidentity.clientId
+if ($LASTEXITCODE -eq 3)
+{
+    write-error "Failed to get the kubelet client id of the cluster"
+    exit $LASTEXITCODE
+}
+# VMSS names list
+$vmss_list = az vmss list --resource-group $node_resource_group --query '[].name' -o tsv
+if ($LASTEXITCODE -eq 3 -or $vmss_list.Length -eq 0)
+{
+    write-error "Failed to get the list of VMSS in the node resource group"
+    exit $LASTEXITCODE
+}
 #######################################################################################################################
 
 PrinitNewSection("Logging in")
 # login with az login.
-#az login
+az login
 
 $subscription = az account show -o tsv --query "id"
 write-host "Extracted subscription <$subscription> successfully"
@@ -59,7 +81,18 @@ PrinitNewSection("Azure addon policy")
 https://docs.microsoft.com/en-us/azure/governance/policy/concepts/policy-for-kubernetes
 Before installing the Azure Policy Add-on or enabling any of the service features, your subscription must enable the Microsoft.PolicyInsights resource providers.
 #>
-if ($should_install_azure_addon_policy)
+# If flag of installation azure addon policy is false , skip this step.
+if ($should_install_azure_addon_policy -eq $false)
+{
+    write-host "Skipping installation of Azure Addon Policy - should_install_azure_addon_policy param is false"
+}
+# If azure addon policy is already installed on cluster, skip this step.
+elseif (az aks show --resource-group $resource_group --name $cluster_name --query addonProfiles.azurepolicy.enabled -eq false)
+{
+    write-host "Skipping installation of Azure Addon Policy - already installed on cluster"
+}
+# Install Azure Addon Policy on cluster.
+else
 {
     write-host "Installing Azure Addon Policy"
     # Provider register: Register the Azure Policy provider
@@ -67,12 +100,13 @@ if ($should_install_azure_addon_policy)
 
     # Enable azure policy addon:
     az aks enable-addons --addons azure-policy --name $cluster_name --resource-group $resource_group
-}
-else
-{
-    write-host "Skipping installation of Azure Addon Policy - should_install_azure_addon_policy param is false"
-}
 
+    if ($LAStEXITCODE -eq 3)
+    {
+        write-error "Failed to enable azure policy addon on cluster"
+        exit $LASTEXITCODE
+    }
+}
 
 #######################################################################################################################
 PrinitNewSection("AzureDefenderInClusterDefense Dependencies")
@@ -86,21 +120,45 @@ az deployment sub create --name  $deployment_name  --location $region `
                                                         location = $region `
                                                         managedIdentityName = $in_cluster_defense_identity_name
 
-# Assign the managed identity to vmss of the cluster
-# az vmss identity assign --resource-group $node_resource_group --name $cluster_name --identity $in_cluster_defense_identity_name
-
+if ($LASTEXITCODE -eq 3)
+{
+    write-error "Failed to create AzureDefenderInClusterDefense dependencies"
+    exit $LASTEXITCODE
+}
 # #####################################################################################################################
 PrinitNewSection("azure-defender-k8s-security-profile Dependencies")
-
-if ($should_enable_aks_security_profile)
+if ($should_enable_aks_security_profile -eq $false)
+{
+    write-host "Skipping on enabling azure-defender-k8s-security-profile - should_install_enable_aks_security_profile param is false"
+}
+elseif (az aks show --resource-group $resource_group --name $cluster_name --query securityProfile.azureDefender.enabled -eq true)
+{
+    write-host "Skipping on enabling of azure-defender-k8s-security-profile - already enabled on cluster"
+}
+else
 {
     ## Enable AKS-AzureDefender Feature flag on your subscription
+    write-host "Enabling of azure-defender-k8s-security-profile"
+
     $url = "https://management.azure.com/subscriptions/$subscription/providers/Microsoft.Features/providers/Microsoft.ContainerService/features/AKS-AzureDefender/register?api-version=2015-12-01";
     $token = (az account get-access-token --query "accessToken" -o tsv)
+
+    if ($LASTEXITCODE -eq 3 -or $token -eq "")
+    {
+        write-error "Failed to get access token"
+        exit $LASTEXITCODE
+    }
+
     $authorization_header = @{
         Authorization = "Bearer $token"
     }
-    Invoke-WebRequest -Method POST -Uri $url -Headers $authorization_header
+    $response = Invoke-WebRequest -Method POST -Uri $url -Headers $authorization_header
+
+    if ($LASTEXITCODE -eq 3 -or $response.StatusCode -ne 200)
+    {
+        write-error "Failed to enable AKS-AzureDefender feature flag"
+        exit $LASTEXITCODE
+    }
 
     # Deploy arm template - containts the dependencies of azure-defender-k8s-security-profile
     # TODO Change the teamplate file to uri.
@@ -113,18 +171,37 @@ if ($should_enable_aks_security_profile)
                                                             clusterResourceGroup = $resource_group `
                                                             resourceLocation = $region
 
+    if ($LASTEXITCODE -eq 3)
+    {
+        write-error "Failed to create azure-defender-k8s-security-profile dependencies"
+        exit $LASTEXITCODE
+    }
 }
-else
-{
-    write-host "Skipping on enabling of azure-defender-k8s-security-profile - should_install_enable_aks_security_profile param is false"
-}
+#######################################################################################################################
+PrinitNewSection("Attach identity to VMSS on node resource group")
 
+For($i = 0; $i -lt $vmss_list.Length; $i++){
+    write-host "Assigning identity to vmss <$vmss_list[$i]>"
+    az vmss identity assign --resource-group $node_resource_group --name $vmss_list[$i] --identities $in_cluster_defense_identity_name
+
+    if ($LASTEXITCODE -eq 3)
+    {
+        write-error "Failed to attach identity to vmss <$vmss_list[$i]>"
+        exit $LASTEXITCODE
+    }
+}
 #######################################################################################################################
 PrinitNewSection("Installing Helm Chart")
 
 # Step 6: Install helm chart
 
 $in_cluster_defense_identity_client_id = az identity show -n $in_cluster_defense_identity_name -g $node_resource_group --query clientId
+
+if ($LASTEXITCODE -eq 3 -or $in_cluster_defense_identity_client_id -eq "")
+{
+    write-error "Failed to get client id of in-cluster-defense identity"
+    exit $LASTEXITCODE
+}
 
 # Switch to current context
 kubectl config use-context $cluster_name
