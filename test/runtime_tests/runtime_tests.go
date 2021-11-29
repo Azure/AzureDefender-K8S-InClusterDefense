bdaf121c35e77b146dd5650967af9e21e2be4bb9
tomer weinberger <tomer.weinberger@microsoft.com>
2021-11-29 17:29:52 +0200

    s

--- test/runtime_tests/runtime_tests.go
+++ test/runtime_tests/runtime_tests.go
@@ -1,13 +1,16 @@
 package runtime_tests
 
-import "github.com/stretchr/testify/suite"
+import (
+	"github.com/stretchr/testify/suite"
+	"net/http"
+)
 
 type RuntimeTestSuite struct {
 	suite.Suite
 }
 
 func (suite *RuntimeTestSuite) SetupTest() {
-	
+	http.Client{}
 }
 
 
